8451b03af7b6f9847e3c9cbe26f733829c237c13
OmarSalhab <omarsalhb24@gmail.com>
2025-05-15 00:30:11 +0300

    feat: implement product seeding and user controller with routes

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,21 @@
+const { Product } = require("../models/Product");
+const asyncHandler = require("express-async-handler");
+
+export const getUsers = (req, res) => {
+	res.send(`Server is running. ${req.headers.host}`);
+}
+
+export const postUser = async (req, res) => {
+	const user = req.body;
+	if (!user.name || !user.email || !user.password)
+		return res
+			.status(400)
+			.json({ success: false, message: "please provide all feilds" });
+	try {
+		const newUser = User(user);
+		await newUser.save();
+		res.status(200).json({ success: true, data: newUser });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "internal server Error" });
+	}
+}
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,26 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("../models/User");
-const asyncHandler = require("express-async-handler");
+const {postUser, getUsers} = require('../controllers/user.controller')
 
-router.get("/", (req, res) => {
-	res.send(`Server is running. ${req.headers.host}`);
-});
 
-router.post("/", async (req, res) => {
-	const user = req.body;
-	if (!user.name || !user.email || !user.password)
-		return res
-			.status(400)
-			.json({ success: false, message: "please provide all feilds" });
-	try {
-		const newUser = User(user);
-		await newUser.save();
-		res.status(200).json({ success: true, data: newUser });
-	} catch (error) {
-		res.status(500).json({ success: false, message: "internal server Error" });
-	}
-});
+
+router.get("/", getUsers);
+
+router.post("/", postUser);
 
 router.delete("/:id", async (req, res) => {
 	const userId = req.params?.id;
